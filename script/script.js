fb30f755f67d3c298cab8c90f31e09b56092e554
Carmine Lentisco <carminelentisco@gmail.com>
2020-05-04 14:50:32 +0200

    JS
    Milestone 1:
     - JavaScript completato.

--- script/script.js
+++ script/script.js
@@ -1,3 +1,42 @@
 jQuery(document).ready(function($) {
+    var inputSearch = $('.input-search');
+    var btnSearch = $('.btn-search');
+    var movieList = $('#movie-list');
+    var source = $('#movie-template').html();
+    var template = Handlebars.compile(source);
     
-}); // End Script
+    btnSearch.click(function () { 
+       var searchText = inputSearch.val();
+       inputSearch.val(' ');
+       
+       $.ajax({
+           url: "https://api.themoviedb.org/3/search/movie?api_key=1f8e3ba9ab80df0fb50bca64de0dc2cc",
+           method: 'GET',
+           data: {
+               query: searchText,
+               language: "it-IT"
+           },
+           success: function (res) {
+                var movieListObj = res.results;
+                
+                for( var key in movieListObj ) {
+                    var movie = {
+                        title: movieListObj[key].title,
+                        originalTitle: movieListObj[key].original_title,
+                        originalLanguage: movieListObj[key].original_language,
+                        vote: movieListObj[key].vote_average
+                    }
+
+                    var html = template(movie);
+                    movieList.append(html);
+                }
+            },
+            error: function () {
+                error('Ops, si è verificato un errore, la pregiamo di riprovare più tardi');
+                alert('Ops, si è verificato un errore, la pregiamo di riprovare più tardi');
+            }
+       });
+
+    });
+
+}); // End Script'
